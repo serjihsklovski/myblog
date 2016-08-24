dc0bbe8d7e1e39b90789b0142c4bd6dbc3edc241
Serjih Sklovski <serjihsklovski@gmail.com>
2016-08-24 17:19:22 +0300

    profile page updated

--- views/user/profile.php
+++ views/user/profile.php
@@ -3,9 +3,30 @@
 
 <head>
     <meta charset="utf-8">
-    <title><?php echo $username; ?></title>
+    <title><?php echo $username; ?></title>\
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">WebSiteName</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Page 1</a></li>
+                <li><a href="#">Page 2</a></li>
+                <li><a href="#">Page 3</a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+<footer>
+
+</footer>
 
 </html>

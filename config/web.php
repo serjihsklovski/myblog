fb0a08f7473488134b577e70b792a6d06c5200db
Serjih Sklovski <serjihsklovski@gmail.com>
2016-10-16 01:19:41 +0300

    database configs added

--- config/web.php
+++ config/web.php
@@ -10,4 +10,11 @@ return [
         'exceptionRoutes' => [],
         'defaultController' => 'main',
     ],
+
+    'database' = [
+        'host' => '127.0.0.1',
+        'name' => 'myblog',
+        'user' => '',
+        'password' => '',
+    ],
 ];

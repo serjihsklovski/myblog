19666efb52ab03b0c65248cbedf8548c982597f8
Serjih Sklovski <serjihsklovski@gmail.com>
2016-10-16 03:07:28 +0300

    refreshed a little

--- views/main/index.php
+++ views/main/index.php
@@ -25,7 +25,7 @@
         </ul>
 
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="/myblog/user/sign-up"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="/user/sign-up"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
             <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
         </ul>
     </div>
--- views/user/sign_up.php
+++ views/user/sign_up.php
@@ -46,30 +46,44 @@
                         </div>
 
                         <div class="panel-body">
-                            <form method="post">
+                            <form method="post" action="/user/sign-up">
+                                <?php
+
+                                if (isset($errors)) {
+                                    echo '<b style="color: darkred">In the registration form the following errors have been found:</b><br>';
+                                    echo '<ul>';
+
+                                    foreach ($errors as $error) {
+                                        echo "<li>$error</li>";
+                                    }
+
+                                    echo'</ul>';
+                                }
+
+                                ?>
                                 <div class="form-group">
                                     <label for="email">Email:</label>
-                                    <input type="email" class="form-control" name="email" maxlength="128" id="email">
+                                    <input type="email" class="form-control" name="email" maxlength="128" id="email" required="required">
                                 </div>
 
                                 <div class="form-group">
                                     <label for="username">Username:</label>
-                                    <input type="text" class="form-control" name="username" maxlength="64" id="username">
+                                    <input type="text" class="form-control" name="username" maxlength="64" id="username" required="required">
                                 </div>
 
                                 <div class="form-group">
                                     <label for="password">Password:</label>
-                                    <input type="password" class="form-control" name="password" maxlength="32" id="password">
+                                    <input type="password" class="form-control" name="password" maxlength="32" id="password" required="required">
                                 </div>
 
                                 <div class="form-group">
                                     <label for="confirm_password">Confirm password:</label>
-                                    <input type="password" class="form-control" name="confirm_password" maxlength="32" id="confirm_password">
+                                    <input type="password" class="form-control" name="confirm_password" maxlength="32" id="confirm_password" required="required">
                                 </div>
 
                                 <div class="form-group">
                                     <div class="checkbox">
-                                        <label><input type="checkbox" value="">Remember me</label>
+                                        <label><input type="checkbox" value="true" name="remember_me">Remember me</label>
                                     </div>
                                 </div>
 

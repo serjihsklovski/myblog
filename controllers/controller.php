ad5e88b37595da512bf6d0881959d4e488081959
Serjih Sklovski <serjihsklovski@gmail.com>
2016-10-16 03:04:07 +0300

    protected field `$_model` and method `__construct` were added

--- controllers/controller.php
+++ controllers/controller.php
@@ -3,6 +3,13 @@
 abstract class Controller
 {
     protected $_defaultAction = 'index';
+    protected $_model;
+
+
+    public function __construct($model)
+    {
+        $this->_model = $model;
+    }
 
 
     public function getDefaultAction()

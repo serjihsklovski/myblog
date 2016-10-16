e4689ea4f0e0f0a9870cd5e1baa7be7f5d6bc292
Serjih Sklovski <serjihsklovski@gmail.com>
2016-10-16 03:22:36 +0300

    `User` model class - methods `usernameExists` and `addUser` were added

--- models/user.php
+++ models/user.php
@@ -26,4 +26,34 @@ class User extends Model
 
         return false;
     }
+
+
+    public function usernameExists($username)
+    {
+        $query = 'SELECT COUNT(*) AS `count` FROM `user` WHERE `username` = :username';
+        $statement = $this->_conn->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+
+        $statement->execute(['username' => $username]);
+
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($result[0]['count'] != 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public function addUser($email, $username, $password)
+    {
+        $query = 'INSERT INTO `user` (`email`, `username`, `password`, `user_status_id`) VALUES (:email, :username, :password, 1)';
+        $statement = $this->_conn->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+
+        $statement->execute([
+            'email' => $email,
+            'username' => $username,
+            'password' => $password
+        ]);
+    }
 }

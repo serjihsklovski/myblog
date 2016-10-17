fe9be9e7dc3db659dbb3eccdb4d8381a56c8bad3
Serjih Sklovski <serjihsklovski@gmail.com>
2016-10-17 10:39:23 +0300

    requires template: navbar

--- views/main/index.php
+++ views/main/index.php
@@ -11,25 +11,7 @@
 
 <body>
 
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">SerjihSklovskiBlog</a>
-        </div>
-
-        <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Main</a></li>
-            <li><a href="#">Page 1</a></li>
-            <li><a href="#">Page 2</a></li>
-            <li><a href="#">Page 3</a></li>
-        </ul>
-
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="/user/sign-up"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-        </ul>
-    </div>
-</nav>
+<?php require ROOT . '/templates/navbar.php' ?>
 
 </body>
 

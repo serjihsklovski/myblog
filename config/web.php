21ce968ff21c1e5f13134f3a029831f933727bf4
Serjih Sklovski <serjihsklovski@gmail.com>
2016-10-17 10:34:58 +0300

    new settings: salt and key for cookies

--- config/web.php
+++ config/web.php
@@ -4,6 +4,9 @@ return [
     'app' => [
         'id' => 'myblog',
         'name' => 'SerjihSklovskiBlog',
+        'passwordLeftSalt' => 'hcsao87e2^',
+        'passwordRightSalt' => 'b9o3e0_278',
+        'cookieKey' => 'ano87dyUGAsodOa8B&^S8wrpe1'
     ],
 
     'router' => [

06caa76d2ff2d9bb35e5683e77589343e79d888f
Serjih Sklovski <serjihsklovski@gmail.com>
2016-10-17 10:42:08 +0300

    new view: `user/login.php`

---
+++ views/user/login.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>My Blog - Login</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <style>
+        .anim {
+            display: none;
+        }
+    </style>
+
+    <script>
+        $(document).ready(function() {
+            $(".anim").fadeIn(500);
+        });
+    </script>
+</head>
+
+<body>
+
+<?php require ROOT . '/templates/navbar.php' ?>
+
+<div class="container">
+    <div style="margin-right: 20%; margin-left: 20%" class="panel panel-default anim">
+        <div class="panel-heading">
+            <h3>Login</h3>
+        </div>
+
+        <div class="panel-body">
+            <?php require ROOT . '/templates/login_form.php' ?>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>

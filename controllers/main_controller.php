43669185d77da01753db2bfa330b8976fd694fae
Serjih Sklovski <serjihsklovski@gmail.com>
2016-10-20 16:12:11 +0300

    class refreshed

--- controllers/main_controller.php
+++ controllers/main_controller.php
@@ -5,8 +5,21 @@ require_once ROOT . '/controllers/controller.php';
 
 class MainController extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct(null);
+    }
+
+
     public function actionIndex()
     {
+        if (
+            Application::getSiteCookie('username') !== null and
+            Application::getSiteCookie('password') !== null
+        ) {
+            $loggedIn = true;
+        }
+        
         require_once ROOT . '/views/main/index.php';
     }
 }

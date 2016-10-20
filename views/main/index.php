11b070dac45208d7821bc691380bb9b1279bdac7
Serjih Sklovski <serjihsklovski@gmail.com>
2016-10-20 19:52:59 +0300

    using bootstrap from assets directory

--- views/main/index.php
+++ views/main/index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <title>My Blog</title>
+    <link rel="stylesheet" href="assets/bootstrap-3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>

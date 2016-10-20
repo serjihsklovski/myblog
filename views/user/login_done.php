28a1471d2de476a472542c5f38cada527fdce38b
Serjih Sklovski <serjihsklovski@gmail.com>
2016-10-20 15:47:56 +0300

    new views: `views/user/{login_done.php, logout.php}`

---
+++ views/user/login_done.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>My Blog - Sign Up Done</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<?php require ROOT . '/templates/navbar.php' ?>
+
+<div class="container">
+    <p>Our congratulations! You have logged in!</p>
+</div>
+
+</body>
+
+</html>
---
+++ views/user/logout.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>My Blog - Login</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<?php require ROOT . '/templates/navbar.php' ?>
+
+<div class="container">
+    <p>Now you are logged out.</p>
+</div>
+
+</body>
+
+</html>
+
